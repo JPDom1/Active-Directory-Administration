44eb5e89d5bd5bd30449e7317a2c54856d28b8aa
JP Erasmus <jean-pierre.erasmus@outlook.com>
2019-03-29 11:29:25 +0000

    Create AD - Computer last logondate.ps1

---
+++ AD - Computer last logondate.ps1
@@ -0,0 +1,13 @@
+﻿<#
+Provide a list of Computers to check last logondate
+JP Erasmus
+29/03/2019
+USE AT OWN RISK!
+#>
+#Import AD Module
+import-module activedirectory  
+
+#Provide list of computers to check last logondate
+Get-Content "C:\path\to\computers\txt"| 
+Get-ADComputer -Properties * | 
+Format-Table Name, lastlogondate 
