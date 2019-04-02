0831fb4adcfb3cd514f5f7711a36d19d70116978
JP Erasmus <jean-pierre.erasmus@outlook.com>
2019-04-02 13:47:54 +0100

    Create AD - user last logon.ps1
    Show user last logon date and time provided from a list running on a foreach loop

---
+++ AD - user last logon.ps1
@@ -0,0 +1,18 @@
+﻿<#
+Provide a list of users to display last logon date and time
+JP Erasmus
+02/04/2019
+Use at own risk
+#>
+#Import the AD Module
+Import-Module activedirectory
+
+#Provide path to TXT with list of users
+$users= Get-Content -Path 'C:\path\to\txt'
+
+#foreach will run and output list of users with lastlogondate
+foreach ($user in $users){
+Get-ADUser $user -Properties lastlogondate |
+
+select name, lastlogondate
+}
