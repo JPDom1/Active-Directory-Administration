2b6614f25a200b86b4fcc52e9e374985ead46127
JP Erasmus <jean-pierre.erasmus@outlook.com>
2019-04-04 10:48:20 +0100

    Create AD - Delete users from list.ps1
    Delete AD users from a txt file

---
+++ AD - Delete users from list.ps1
@@ -0,0 +1,22 @@
+﻿<#
+Delete AD users from list.
+JP Erasmus 04/04/2019
+Use at own Risk!
+#>
+
+
+#Prompt for administration credentials with write access to AD
+
+$credentials = Get-Credential -Credential 
+
+#Path to file with users to delete
+$users = Get-Content -Path "c:\scripts\userstodelete.txt"
+
+#Loop to delete each user.
+foreach ($user in $users) {
+
+Remove-ADUser -Credential $credentials -Identity $user -Verbose 
+
+Write-Host "$user has been deleted from AD" -BackgroundColor "Green" -ForegroundColor "Black"
+}
+
